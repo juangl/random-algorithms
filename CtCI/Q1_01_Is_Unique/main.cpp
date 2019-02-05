25ab578c3c5d2515c3631b22ea4af3e821abcaf5
Juan Je Garcia <juanjegarcialopez@gmail.com>
2019-02-05 01:59:46 -0600

    range based loop

--- CtCI/Q1_01_Is_Unique/main.cpp
+++ CtCI/Q1_01_Is_Unique/main.cpp
@@ -22,11 +22,11 @@ bool uniqueCharsNoDS(const char* str) {
 }
 
 int main() {
-  const char* const strs[3] = {"uniq chars", "no unique", "no unique either"};
+  const char* const strs[] = {"uniq chars", "no unique", "no unique either"};
 
   cout << "without additional data structures: " << endl;
-  for (int i = 0; i < 3; i++) {
-    cout << strs[i] << ": " << boolalpha << uniqueCharsNoDS(strs[i]) << endl;
+  for (auto e : strs) {
+    cout << e << ": " << boolalpha << uniqueCharsNoDS(e) << endl;
   }
   return 0;
 }
